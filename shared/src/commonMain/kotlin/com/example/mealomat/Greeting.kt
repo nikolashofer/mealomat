eb47d6e9a2d96db4b8161c7b0751822e0694701d
Nikolas Hofer <nikolas.hofer@icloud.com>
2026-08-21 09:49:16 +0200

    chore: remove scaffold files

--- shared/src/commonMain/kotlin/com/example/mealomat/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mealomat
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return sayHello(platform.name)
-    }
-}
--- shared/src/commonMain/kotlin/com/example/mealomat/GreetingUtil.kt
+++
@@ -1,4 +0,0 @@
-package com.example.mealomat
-
-fun sayHello(to: String): String =
-    "Hello, $to!"
